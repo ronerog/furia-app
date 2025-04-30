e536a0b4a95cb3fa59c5f7ee789d2c8205574f78
Rônero <ronerojr@gmail.com>
2025-04-30 15:26:38 -0300

    fix: tirando caracter perdido na pagina e tamanho min senha

--- src/pages/Register.tsx
+++ src/pages/Register.tsx
@@ -69,7 +69,7 @@ const schemas = [accountSchema, personalInfoSchema, fanInfoSchema];
 const registerSchema = yup.object().shape({
     username: yup.string().required('O nome de usuário é obrigatório'),
     email: yup.string().email('E-mail inválido').required('O e-mail é obrigatório'),
-    password: yup.string().required('A senha é obrigatória'),
+    password: yup.string().required('A senha é obrigatória').min(6, 'A senha deve ter no mínimo 6 caracteres'),
     confirmPassword: yup
       .string()
       .oneOf([yup.ref('password')], 'As senhas devem coincidir')
@@ -240,7 +240,7 @@ const PersonalInfoStep = () => {
       </Typography>
       
       <Grid container spacing={2}>
-        <Grid size={{md: 12}}>ss
+        <Grid size={{md: 12}}>
           <Controller
             name="fullName"
             control={control}
