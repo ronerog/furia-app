7a06aae5ffebc2412294bfbf7f9075b669010498
Rônero <ronerojr@gmail.com>
2025-05-01 16:54:54 -0300

    fix: url socket

--- src/config.ts
+++ src/config.ts
@@ -1,2 +1,2 @@
 export const API_URL = import.meta.env.API_URL || 'https://furia-backend.onrender.com/api';
-export const SOCKET_URL = import.meta.env.SOCKET_URL || 'http://localhost:5000/';
+export const SOCKET_URL = import.meta.env.SOCKET_URL || 'https://furia-backend.onrender.com';
